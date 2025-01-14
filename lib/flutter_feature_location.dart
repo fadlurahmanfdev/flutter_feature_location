f38c59832ecc1fdceef54945d088690e749c583f
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2025-01-14 07:39:59 +0700

    remove root library flutter_feature_location

--- lib/flutter_feature_location.dart
+++
@@ -1,8 +0,0 @@
-
-import 'flutter_feature_location_platform_interface.dart';
-
-class FlutterFeatureLocation {
-  Future<String?> getPlatformVersion() {
-    return FlutterFeatureLocationPlatform.instance.getPlatformVersion();
-  }
-}
